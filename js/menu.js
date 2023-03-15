2342765032b2456b27f524895d54a8f24b413712
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-03-15 18:26:56 +0530

    Modify menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('.hamItems');
 const mobileNav = document.querySelector('.mob-nav');
 const ham = document.querySelector('.hamburger');
 const closeBtn = document.querySelector('.closeBtn');
+const hide = document.querySelector('.footer-social-icons-container');
 
 menu.classList.add('invisible');
 
@@ -9,14 +10,19 @@ menu.classList.add('invisible');
 ham.addEventListener('click', () => {
   menu.classList.remove('invisible');
   mobileNav.classList.add('invisible');
+  hide.classList.add('invisible');
 });
 // Event Listener for close button
 closeBtn.addEventListener('click', () => {
   mobileNav.classList.remove('invisible');
   menu.classList.add('invisible');
+  hide.classList.remove('invisible');
 });
 // Event Listener for menu items
 menu.addEventListener('click', () => {
   menu.classList.add('invisible');
   mobileNav.classList.remove('invisible');
+  hide.classList.remove('invisible');
 });
+
+
